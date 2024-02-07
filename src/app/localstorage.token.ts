0593a7a9618195a4d1e54b4472f49919a2f76c6d
X-KG <krishnaggupta8@gmail.com>
2024-02-07 21:02:24 +0530

    working on localstorage

--- src/app/localstorage.token.ts
+++ src/app/localstorage.token.ts
@@ -1,8 +1,13 @@
-import { InjectionToken } from "@angular/core";
+import { InjectionToken, PLATFORM_ID, inject } from "@angular/core";
 
-export const localStorageToken = new InjectionToken<any>('local storage', {
-    providedIn: 'root',
-    factory() {
-        return localStorage;
-    },
-});
+export const localStorageToken = new InjectionToken<Storage>(
+    'local storage',
+    {
+      providedIn: 'root',
+      factory: () => {
+        return inject(PLATFORM_ID) === 'browser'
+          ? window.localStorage
+          : ({} as Storage);
+      },
+    }
+  );
