686b7c9485310ac993973ea6a5de7fd89332848e
X-KG <krishnaggupta8@gmail.com>
2024-04-08 20:16:48 +0530

    implemented lazy loading for certain components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,27 @@ import { NotfoundComponent } from './notfound/notfound.component';
 import { RoomsBookingComponent } from './rooms/rooms-booking/rooms-booking.component';
 import { RoomsAddComponent } from './rooms/rooms-add/rooms-add.component';
 import { LoginComponent } from './login/login.component';
+import { BookingComponent } from './booking/booking.component';
 
 export const routes: Routes = [
   { path: 'employee', component: EmployeeComponent },
+  // { path: 'booking', component: BookingComponent }, // without lazy loading
+  {
+    path: 'booking',
+    loadComponent: () =>
+      import('./booking/booking.component').then((m) => m.BookingComponent),
+  },
   {
     path: 'rooms',
-    component: RoomsComponent,
+    loadComponent: () =>
+      import('./rooms/rooms.component').then((m) => m.RoomsComponent),
     children: [
-      { path: 'add', component: RoomsAddComponent },
-      { path: ':roomid', component: RoomsBookingComponent },
+      { path: 'add', 
+      loadComponent:() => 
+      import('./rooms/rooms-add/rooms-add.component').then((m) => m.RoomsAddComponent)},
+      { path: ':roomid', 
+      loadComponent: () => 
+      import('./rooms/rooms-booking/rooms-booking.component').then((m) => m.RoomsBookingComponent)},
     ],
   },
   { path: 'login', component: LoginComponent },
